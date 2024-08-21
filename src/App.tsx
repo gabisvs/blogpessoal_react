c4f06c416e7e1b9f046d6bb173ad8fa94d36a1be
Gabrielli <gabivirgilio@oultook.com>
2024-08-21 12:10:34 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Home from "./pages/home/home"
+
+
+function App() {
+
+  // Código TypeScript (fora do return)
+
+  return (
+    // Código TSX => HTML + CSS
+    <>
+      <Home />
+      
+    </>
+  )
+}
+
+export default App
